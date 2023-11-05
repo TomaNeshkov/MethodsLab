a8a942c28b58728852aa0399707b22bceaca3697
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-05 21:39:38 +0200

    Exercise 2 (100/100)

--- 1. Sign of Integer Numbers/1. Sign of Integer Numbers/Program.cs
+++ 1. Sign of Integer Numbers/1. Sign of Integer Numbers/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintSign(number);
+    }
+
+    static void PrintSign(int n)
+    {
+        if (n > 0)
+        {
+            Console.WriteLine($"The number {n} is positive.");
+        }
+        else if (n < 0)
+        {
+            Console.WriteLine($"The number {n} is negative.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {n} is zero.");
+        }
+    }
+}
