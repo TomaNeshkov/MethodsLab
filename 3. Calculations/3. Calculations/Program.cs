0521b7f12edd9ba14a7e2274b55469de3a91b2ca
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-05 21:48:17 +0200

    Exercises (100/100)

---
+++ 3. Calculations/3. Calculations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string operation = Console.ReadLine();
+        double number1 = double.Parse(Console.ReadLine());
+        double number2 = double.Parse(Console.ReadLine());
+
+        switch (operation)
+        {
+            case "add":
+                Add(number1, number2);
+                break;
+            case "multiply":
+                Multiply(number1, number2);
+                break;
+            case "subtract":
+                Subtract(number1, number2);
+                break;
+            case "divide":
+                Divide(number1, number2);
+                break;
+        }
+    }
+
+    static void Add(double num1, double num2)
+    {
+        Console.WriteLine(num1 + num2);
+    }
+
+    static void Multiply(double num1, double num2)
+    {
+        Console.WriteLine(num1 * num2);
+    }
+
+    static void Subtract(double num1, double num2)
+    {
+        Console.WriteLine(num1 - num2);
+    }
+
+    static void Divide(double num1, double num2)
+    {
+        if (num2 != 0)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
